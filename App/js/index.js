3f9de7cf81f682a57482be0f2c67c8145e1c3353
dheerajrikku <59282461+dheerajrikku@users.noreply.github.com>
2020-08-24 20:14:28 +0530

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,47 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"xui_ui_comboinput3")
+                .setDirtyMark(false)
+                .setLeft("4.571428571428571em")
+                .setTop("9.904761904761905em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Number Input")
+                .setType("number")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button3")
+                .setDirtyMark(false)
+                .setLeft("25.142857142857142em")
+                .setTop("9.904761904761905em")
+                .setCaption("Button")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button6")
+                .setDirtyMark(false)
+                .setLeft("12.952380952380953em")
+                .setTop("25.904761904761905em")
+                .setWidth("44.19047619047619em")
+                .setCaption("Status Button")
+                .setType("status")
+            );
+            
+            append(
+                xui.create("xui.UI.ProgressBar")
+                .setHost(host,"xui_ui_progressbar2")
+                .setDirtyMark(false)
+                .setLeft("21.333333333333332em")
+                .setTop("32.76190476190476em")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
